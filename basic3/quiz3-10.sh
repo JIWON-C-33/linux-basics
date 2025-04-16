c111eb8861dd67afa418de03a842ce407f7d6fcf
JIWON-C-33 <cheonji1130@gmail.com>
2025-04-16 12:15:16 +0900

    done

--- basic3/quiz3-10.sh
+++ basic3/quiz3-10.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
+file_count=0
+dir_count=0
 
+for item in *
+do
+	if [ -f "$item" ]
+	then
+		f_count=$((f_count+1))
+	elif [ -d "$item" ]
+	then 
+		d_count=$((d_count+1))
+	else
+		echo "$item x"
+	fi
+done
+
+echo "파일 수: $f_count"
+echo "디렉토리 수: $d_count"
+
+exit 0
